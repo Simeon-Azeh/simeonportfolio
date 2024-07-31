a5ecae8541656eeb5b76f99eb4e6912fccc48b8a
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-31 23:59:37 +0200

    some work

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import work1 from '../../public/images/work1.png';
+import work2 from '../../public/images/work2.png';
+import work3 from '../../public/images/work3.png';
+import work4 from '../../public/images/work4.png';
+import work5 from '../../public/images/work5.png';
+import work6 from '../../public/images/work6.png';
+import work7 from '../../public/images/work7.png';
 
-const images = [
-  '../../public/images/work1.png',
-  '../../public/images/work2.png',
-  '../../public/images/work3.png',
-  '../../public/images/work4.png',
-  '../../public/images/work5.png',
-  '../../public/images/work6.png',
-  '../../public/images/work7.png',
-];
+const images = [work1, work2, work3, work4, work5, work6, work7];
 
 const MyWork = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length);
+      setFadeIn(false); // Trigger fade-out
+      setTimeout(() => {
+        setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length);
+        setFadeIn(true); // Trigger fade-in
+      }, 500); // Duration of fade-out before switching image
     }, 3000); // Change image every 3 seconds
 
     return () => clearInterval(interval);
@@ -24,27 +28,26 @@ const MyWork = () => {
 
   return (
     <div className='bg-[#f9feff] dark:bg-dark-body transition-colors font-inter'>
-              <div className=" w-full md:w-4/5 mx-auto py-10 px-6 md:px-0 font-inter">
-      <h2 className=" font-medium mb-2 text-[#414760] dark:text-gray-200 ">
-        My Work
-      </h2>
-      <div className="flex flex-col items-center">
-        <div className="w-full h-96 relative overflow-hidden border rounded-lg shadow-lg">
-          <img
-            src={images[currentImageIndex]}
-            alt="My Work"
-            className="w-full h-full object-cover rounded-lg transition-opacity duration-1000"
-          />
-          <Link to="/portfolio" className="absolute bottom-[40%] left-1/2 transform -translate-x-1/2">
-            <button className="dark:bg-dark-body bg-orange-500 text-white px-4 py-2 rounded-lg hover:translate-y-[-3px] duration-300">
-              View Work
-            </button>
-          </Link>
+      <div className=" w-full md:w-4/5 mx-auto py-10 px-6 md:px-0 font-inter">
+        <h2 className=" font-medium mb-2 text-[#414760] dark:text-gray-200 ">
+          My Work
+        </h2>
+        <div className="flex flex-col items-center">
+          <div className="w-full h-96 relative overflow-hidden border rounded-lg shadow-lg">
+            <img
+              src={images[currentImageIndex]}
+              alt="My Work"
+              className={`w-full h-full object-cover rounded-lg transition-opacity duration-500 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
+            />
+            <Link to="/portfolio" className="absolute bottom-[40%] left-1/2 transform -translate-x-1/2">
+              <button className="dark:bg-dark-body bg-orange-500 text-white px-4 py-2 rounded-lg hover:translate-y-[-3px] duration-300">
+                View Work
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  
   );
 };
 
